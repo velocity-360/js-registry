953f12bdf33ef0266fa1b6ab310a875182b1d79d
fullstack360 <info@fullstack360.com>
2017-02-18 12:50:46 -0500

    static file path

--- server/app.js
+++ server/app.js
@@ -1,6 +1,11 @@
 var express = require('express')
+var path = require('path')
+var logger = require('morgan')
 
 var app = express()
+app.use(logger('dev'))
+app.use(express.static(path.join(__dirname, 'assets')))
+
 app.get('/', function(req, res, next){
 
 	res.send('TEST')
