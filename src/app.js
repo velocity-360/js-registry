3dc12f0e88cd63a30d9e5f3f9d7a9f095937d87b
fullstack360 <info@fullstack360.com>
2017-02-18 10:46:21 -0500

    initial

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+const app = (
+	<div>
+		Entry Point
+	</div>
+)
+
+ReactDOM.render(app, document.getElementById('root'))
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var webpack = require('webpack')
+var path = require('path')
+
+module.exports = {
+
+	entry: {
+		app: './src/app.js'
+	},
+	output: {
+		filename: 'dist/bundle.js',
+		sourceMapFilename: 'dist/bundle.map'
+	},
+	devtool: '#source-map',
+	module: {
+		loaders: [
+			{
+				exclude: /(node_modules)/,
+				loader: 'babel-loader',
+				query: {
+					presets: ['react', 'es2015']
+				}
+			}
+		]
+	}
+
+}
