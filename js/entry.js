ef92de0be78fd2cd4bc58ffbd9cd0edea9a9356b
fullstack360 <info@fullstack360.com>
2017-02-18 11:56:22 -0500

    js entry point

--- js/entry.js
+++ js/entry.js
@@ -4,7 +4,8 @@ var root = document.createElement('div')
 root.setAttribute('id', 'root')
 document.body.appendChild(root)
 
+// load demo script..
 var script = document.createElement('script')
-script.setAttribute('src', 'root')
+script.setAttribute('src', 'https://raw.githubusercontent.com/velocity-360/js-registry/master/demo/example.js')
 document.body.appendChild(script)
 
