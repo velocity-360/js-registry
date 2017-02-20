02bcebd41181aa98945c68c0570395c71586f220
fullstack360 <info@fullstack360.com>
2017-02-20 15:11:37 -0500

    set up templating engine

--- server/app.js
+++ server/app.js
@@ -1,8 +1,21 @@
 var express = require('express')
 var path = require('path')
 var logger = require('morgan')
+var favicon = require('serve-favicon')
+var cookieParser = require('cookie-parser')
+var bodyParser = require('body-parser')
+var sessions = require('client-sessions')
+var mongoose = require('mongoose')
+var compression = require('compression')
+
 
 var app = express()
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'mustache')
+app.engine('mustache', require('hogan-middleware').__express)
+
+
 app.use(logger('dev'))
 var staticPath = path.join(__dirname, 'public').replace('server/', '')
 //console.log('Static Path: '+static_path)
@@ -10,7 +23,7 @@ app.use(express.static(staticPath))
 
 app.get('/', function(req, res, next){
 
-	res.send('TEST')
+	res.render('index', null)
 
 })
 
