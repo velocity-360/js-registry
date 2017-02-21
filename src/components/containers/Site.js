87eee5bbe5dcdaaec090d4c68892bbf371071ec3
fullstack360 <info@fullstack360.com>
2017-02-21 13:36:03 -0500

    show site tag

--- src/components/containers/Site.js
+++ src/components/containers/Site.js
@@ -2,24 +2,42 @@ import React, { Component } from 'react'
 import { APIManager } from '../../utils'
 
 class Site extends Component {
+	constructor(){
+		super()
+		this.state = {
+			site: null
+		}
+	}
 
 	componentDidMount(){
 		APIManager
 		.handleGet('/account/currentuser')
 		.then(response => {
 			console.log(JSON.stringify(response))
-			
+			this.setState({
+				site: response.site
+			})
+
 		})
 		.catch(err => {
 			console.log(err.message)
 		})
-		
-
 	}
 
 	render(){
-		return (
-			<div>Site Container</div>
+		const site = this.state.site
+		const scriptTag = (site == null ) ? null : '<script src="http://54.158.144.8/library/'+site.id+'"></script>'
+
+		return (site == null ) ? <div></div> : (
+			<div>
+				<h3>{site.name}</h3>
+				<span>Copy and paste the code below into your site before the end of the body tag:</span>
+				<br />
+				<code style={{background:'#f9f9f9', padding:16, border:'1px solid #ddd'}}>
+					{scriptTag}
+
+				</code>
+			</div>
 		)
 	}
 }
