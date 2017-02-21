be2fffb10f48eae5d5b1a496ba6d48b662fc9a97
fullstack360 <info@fullstack360.com>
2017-02-21 15:43:03 -0500

    subscribers array on module

--- server/models/Module.js
+++ server/models/Module.js
@@ -4,6 +4,7 @@ var ModuleSchema = new mongoose.Schema({
 	name: {type:String, trim:true, default:''},
 	slug: {type:String, trim:true, default:''},
 	cdn: {type:String, trim:true, default:''},
+	subscribers: {type:Array, default:[]},
 	image: {type:String, trim:true, default:''},
 	timestamp: {type:Date, default:Date.now}
 })
@@ -13,6 +14,7 @@ ModuleSchema.methods.summary = function() {
 		name:this.name,
 		slug:this.slug,
 		cdn:this.cdn,
+		subscribers:this.subscribers,
 		image:this.image,
 		timestamp:this.timestamp,
 		id:this._id.toString()
