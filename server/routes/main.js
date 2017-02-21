aaab474a83a236c172d07dcab7d664260718ef4e
fullstack360 <info@fullstack360.com>
2017-02-21 11:40:01 -0500

    redirect to register page if no session

--- server/routes/main.js
+++ server/routes/main.js
@@ -2,6 +2,15 @@ var express = require('express')
 var router = express.Router()
 
 router.get('/', function(req, res, next){
+	if (req.session == null){
+		res.redirect('/register')
+		return
+	}
+
+	if (req.session.site == null){
+		res.redirect('/register')
+		return
+	}
 
 	res.render('index', null)
 
