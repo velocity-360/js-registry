8ec8dea4dcb1d84e5bfc4efb060b049333940f6f
fullstack360 <info@fullstack360.com>
2017-02-21 13:53:53 -0500

    typos

--- server/routes/library.js
+++ server/routes/library.js
@@ -6,7 +6,7 @@ router.get('/:site', function(req, res, next){
 	var site = req.params.site
 
 	controllers.site
-	.findById()
+	.getById(site, false)
 	.then(function(site){
 		res.setHeader('Content-Type', 'text/javascript')
 		res.send("console.log('HELLO " + site.name + "')")
