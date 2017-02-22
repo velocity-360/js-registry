ec6b3f227ccb1660c688211c10dd9b2449a36efe
fullstack360 <info@fullstack360.com>
2017-02-22 00:11:32 -0500

    pre load react

--- server/routes/library.js
+++ server/routes/library.js
@@ -6,18 +6,27 @@ var Request = require('../utils/Request')
 router.get('/:site', function(req, res, next){
 	var site = req.params.site
 
+	var js = ''
+
 	controllers.site
 	.getById(site, false)
 	.then(function(site){
-		var url = 'https://thevarsity.s3.amazonaws.com/HGoxdM0x-bundle.7989f1b2e676f47baa2f.js'
-		return Request.handleGet(url)
+		var react = 'http://54.158.144.8/dist/react/react-react-dom.min.js'
+		return Request.handleGet(react)
 	})
 	.then(function(text){
+		js += text
+		var url = 'https://thevarsity.s3.amazonaws.com/HGoxdM0x-bundle.7989f1b2e676f47baa2f.js'
+		return Request.handleGet(url)
+
+	})
+	.then(function(text){
+		js += text
 //		console.log(text)
 //		res.send("console.log('HELLO " + site.name + "')")
 
 		res.setHeader('Content-Type', 'text/javascript')
-		res.send(text)
+		res.send(js)
 	})
 	.catch(function(err){
 		res.send('ERROR: '+err.message)
